5b9a1c41fe81726d58e8c38a3682554a5449f39c
Tall-Fellow <jesper@fullersta.com>
2023-03-21 19:14:33 +0100

    Added comments

--- concentration_game/js/jquery.flipcard.js
+++ concentration_game/js/jquery.flipcard.js
@@ -2,15 +2,16 @@
  * @file
  */
 
+// This code was mostly supplied to us by Karde, our changes will be commented below.
 ( function( $ ) {
   $.fn.flipcard = function( params ) {
     const Memory = {
       $game: null,
-      hintCount: 0,
-      guesses: 0,
-      startTime: 0,
-      stopTime: 0,
-      hintStat: 0,
+      hintCount: 0, // Tracks failed player guesses, used for hints
+      guesses: 0,   // Tracks player guesses, used for stats
+      startTime: 0, // Starts on first card clicked, used for stats
+      stopTime: 0,  // When final pair is found, used for stats
+      hintStat: 0,  // Amount of hints given, used for stats
       init( el, options ) {
         const shuffledCards = this.shuffle( options.cards );
         const slicedCards = shuffledCards.slice( 0, options.limit );
@@ -30,7 +31,7 @@
       setup( el ) {
         el.html( this.buildHTML() );
         var stats = document.getElementById("stats");
-        stats.style.display = "none";
+        stats.style.display = "none"; // Hide stats on game start
       },
 
       binding( el ) {
@@ -61,12 +62,14 @@
           // if its clicked the first time, start guessing.
           if (!_.getData( $card, "guess" )) {
             _.setData( $card, "guess", $( this ).attr( "data-id" ) );
-            _.guesses ++;
+            _.guesses++;
             if (_.startTime == 0) {
               _.startTime = Date.now();
             }
+
+            // If enough failed attempts, give hint
             if (_.hintCount >= hintLimit) {
-              //This saves the matching card of the currently picked one, for further use
+              // This saves the matching card of the currently picked one, for further use
               $hintCard = $(this).siblings().filter(function(i) {
                 return $(this).attr("data-id") === _.getData( $card, "guess"); //Scan through all cards until true match is found
               });
@@ -76,27 +79,27 @@
             }
           } 
           
-          // if we are guessing and right card is clicked, set a match and start guessing next.
+          // If we are guessing and right card is clicked, set a match and start guessing next.
           else if ( _.getData( $card, "guess" ) === $( this ).attr( "data-id" ) && !$( this ).hasClass( "picked" ) ) {
             $card.closest( ".flipcard" ).find( ".picked" ).addClass( "matched" );
             _.setData( $card, "guess", null );
-            _.hintCount = 0;
+            _.hintCount = 0; // Resets failed attempt count
             $(".hint").removeClass("hint"); // Removes hint class
           } 
           
-          // its not a correct guess, reset both the cards and start guessing again.
+          // It's not a correct guess, reset both the cards and start guessing again.
           else {
             _.hintCount++;
             _.setData( $card, "guess", null );
             _.setData( $card, "paused", true );
+            $(".hint").removeClass("hint");
             setTimeout( () => {
               $card.closest( ".flipcard" ).find( ".picked" ).removeClass( "picked" );
               _.setData( $card, "paused", false );
-            }, 600 );
-            $(".hint").removeClass("hint");
+            }, 600);
           }
           
-          // flag success image when all cards are identified.
+          // Flag success image when all cards are identified.
           if (
             $card
             .closest( ".flipcard" )
@@ -117,24 +120,26 @@
         
         $card.closest( ".flipcard" ).find( ".card" ).hide();
         overlay.show();
-        _.stopTime = (Date.now() - _.startTime)/1000;
+        _.stopTime = (Date.now() - _.startTime)/1000; // Saves final time as seconds
         _.setData( $card, "paused", true );
        
-        // Start save sats
+        // Initialize user stats
         if (localStorage.getItem('game_stats') === null) {
           localStorage.setItem('game_stats', '');  
         }
         
+        // Build stats into save string format and save
         let game_stats = localStorage.getItem('game_stats') + user_id + "|" + _.stopTime + "|" + _.guesses + "|" + _.hintStat + "|" + cardAmount + ";";
         localStorage.setItem('game_stats', game_stats);
-        // End save stats
 
+        // Make stats visible, reset stat counters
         drawStats();
         var stats = document.getElementById("stats");
         stats.style.display = "block";
         _.guesses = 0;
         _.startTime = 0;
 
+        // Make win buttons clickable after some time
         setTimeout( () => {
         $( `[data-game="#${blockId}"]` ).prop( "disabled", false ).removeClass("btn-disabled");
         $( `._${blockId}_button` ).prop( "disabled", false ).removeClass( "btn-disabled" );
@@ -185,6 +190,10 @@
       }
     };
     
+    // Sets up the board layout
+    // The reason for 2x2, 3x2, 4x4 here and 
+    // twobytwo, threebytwo and fourbyfour in
+    // .twig is because of CSS rules
     switch (cardAmount) {
       case "2x2":
         t = 2;
@@ -205,7 +214,7 @@
 
     params = $.extend( {
       success: null,
-      limit: t,
+      limit: t, // Sets amount of cards = (t*2)
       cards: []
     }, params );
 
